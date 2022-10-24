85263b2c94d7a0ef889211bf312d32791a4dd962
raorajat21 <rao.rajat@northeastern.edu>
2022-10-23 21:13:47 -0400

    modifying ami file

--- ami_packer/setup.sh
+++ ami_packer/setup.sh
@@ -9,8 +9,8 @@ curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 sudo apt-get install unzip -y
-cd ~/ && unzip webappDEV.zip
-cd ~/webappDEV && npm i
+cd ~/ && unzip WebAppDEV.zip
+cd ~/WebAppDEV && npm i
 
 sudo mv /tmp/webapp.service  /etc/systemd/system/webapp.service
 sudo systemctl enable webapp.service
@@ -19,5 +19,3 @@ sudo systemctl start webapp.service
 sudo apt-get update -y
 sudo apt-get install -y  mysql-server
 
-#sudo apt-get install mysql-client --fix-missing --fix-broken -y
-#ami-04b3c39a8a1c62b76
