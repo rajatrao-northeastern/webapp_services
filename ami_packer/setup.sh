3f443a3a42c6109ff8f4d597ffcaef409b0ed12d
raorajat21 <98132552+raorajat21@users.noreply.github.com>
2022-10-26 22:38:13 -0400

    updating sh

--- ami_packer/setup.sh
+++ ami_packer/setup.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
-
 sleep 30
-
 sudo apt-get update -y
+sudo apt-get install zip unzip
+sudo apt update && sudo apt install --assume-yes curl
 sudo apt-get install nginx -y
-
-curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-sudo apt-get install unzip -y
-cd ~/ && unzip WebAppDEV.zip
-cd ~/WebAppDEV && npm i
-
-sudo mv /tmp/webapp.service  /etc/systemd/system/webapp.service
-sudo systemctl enable webapp.service
-sudo systemctl start webapp.service
-
-sudo apt-get update -y
-sudo apt-get install -y  mysql-server
-
+# curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+sudo apt -y install nodejs
+sudo apt-get -y install npm
+echo "Installing mysql server"
+sudo apt-get install mysql-server -y
+sudo mysql <<EOF
+CREATE DATABASE usersDB;
+CREATE USER 'rajat'@'localhost' IDENTIFIED BY 'Rajatrao@0594';
+GRANT ALL PRIVILEGES ON usersDB.* TO 'rajat'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
+echo "Starting mysql server"
+sudo service mysql start
+sudo apt-get install -y gcc g++ make
+sudo npm i pm2 -g
+sleep 10
+unzip WebAppDEV.zip
+cd WebAppDEV/ && npm i
+sudo pm2 start index.js
+sudo pm2 save
+sudo pm2 startup systemd
